548c8b7002ffd9b57af5481d7e0b84c0756fdf4b
Stephy <stephy271199@gmail.com>
2022-10-08 14:16:34 +0800

    adjust layout

--- week3/task-1/assignment-3.py
+++ week3/task-1/assignment-3.py
@@ -1,4 +1,3 @@
-
 import urllib.request as request
 import json
 src = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
@@ -10,9 +9,9 @@ with request.urlopen(src) as response:
             imgUrl = tpTrip["file"].split("https")
             print("https"+imgUrl[1])
 
-    with open("data.csv", "w", encoding="utf-8") as file:
-        for tpTrip in tpData:
-            if (tpTrip["xpostDate"][0:4]) >= "2015":
-                file.write(tpTrip["stitle"]+","+tpTrip["address"][5:8]+"," +
-                           tpTrip["longitude"]+","+tpTrip["latitude"]+"," +
-                           "https"+tpTrip["file"].split("https")[1]+"\n")
+with open("data.csv", "w", encoding="utf-8") as file:
+    for tpTrip in tpData:
+        if (tpTrip["xpostDate"][0:4]) >= "2015":
+            file.write(tpTrip["stitle"]+","+tpTrip["address"][5:8]+"," +
+                       tpTrip["longitude"]+","+tpTrip["latitude"]+"," +
+                       "https"+tpTrip["file"].split("https")[1]+"\n")
