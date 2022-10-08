1ee1fb3f396fc8cb81d91c8f9d4c67d41e347bce
Stephy <stephy271199@gmail.com>
2022-10-08 14:06:19 +0800

    adjust layout

--- week3/task-1/assignment-3.py
+++ week3/task-1/assignment-3.py
@@ -4,24 +4,15 @@ import json
 src = "https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
 with request.urlopen(src) as response:
     data = json.load(response)
-tpattract = data["result"]["results"]
-for tptrip in tpattract:
-    # if (tptrip["xpostDate"][0:4]) >= "2015":
-    if (tptrip["xpostDate"][0:4]) >= "2015":
-        # print(tptrip["xpostDate"][0:4])
-        # print(len(tptrip))
-        # print(tptrip["address"][5:8])
-        # print(len(tptrip))
-        # print(tptrip["file"])
-        # print(tptrip["file"].split("jpg"))
-        # print(len(tptrip["file"]))
-        imgurl = tptrip["file"].split("https")
-        print("https"+imgurl[1])
-        # print(imgurl)
+    tpData = data["result"]["results"]
+    for tpTrip in tpData:
+        if (tpTrip["xpostDate"][0:4]) >= "2015":
+            imgUrl = tpTrip["file"].split("https")
+            print("https"+imgUrl[1])
 
     with open("data.csv", "w", encoding="utf-8") as file:
-        for tptrip in tpattract:
-            if (tptrip["xpostDate"][0:4]) >= "2015":
-                file.write(tptrip["stitle"]+","+tptrip["address"][5:8]+"," +
-                           tptrip["longitude"]+","+tptrip["latitude"]+"," +
-                           "https"+tptrip["file"].split("https")[1]+"\n")
+        for tpTrip in tpData:
+            if (tpTrip["xpostDate"][0:4]) >= "2015":
+                file.write(tpTrip["stitle"]+","+tpTrip["address"][5:8]+"," +
+                           tpTrip["longitude"]+","+tpTrip["latitude"]+"," +
+                           "https"+tpTrip["file"].split("https")[1]+"\n")
