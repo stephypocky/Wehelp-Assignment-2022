46749b9d427236c0b011aa077ce85676bf9dbff2
Stephy <stephy271199@gmail.com>
2022-10-18 20:27:29 +0800

    first commit

---
+++ week-5/README.md
@@ -0,0 +1,112 @@
+# Assignment - Week 5
+
+## Install
+
+This project uses MySQL. Go check them out if you don't have them locally installed.
+
+`https://www.mysql.com/.`
+
+## Content
+
+### Task 3
+
+1.
+
+```
+insert into member(id, name, username, password, follower_count, time)values (1, 'Amy', 'test', 'test', 100, '2008-08-08 22:10:40');
+insert into member(id, name, username, password, follower_count, time)values (2, 'Ben', 'aaa', 'aaa', 150, '2011-02-11 11:18:20');
+insert into member(id, name, username, password, follower_count, time)values (3, 'Charlie', 'bbb', 'bbb', 20, '2003-12-01 03:55:50');
+insert into member(id, name, username, password, follower_count, time)values (4, 'Doris', 'ccc', 'ccc', 450, '2020-01-01 15:35:10');
+insert into member(id, name, username, password, follower_count, time)values (5, 'Eric', 'ddd', 'ddd', 80, '2009-03-03 09:30:00');
+```
+
+![](./screenshots/Task3_01.png)
+
+2.
+
+```select * from member;```
+
+![](./screenshots/Task3_02.png)
+
+3.
+
+```select * from member order by time desc;```
+
+![](./screenshots/Task3_03.png)
+
+4.
+
+```select * from member order by time desc limit 1,3;```
+
+![](./screenshots/Task3_04.png)
+
+5.
+
+```select * from member where username='test';```
+
+![](./screenshots/Task3_05.png)
+
+6.
+
+ ```select * from member where username='test' and password='test';```
+
+![](./screenshots/Task3_06.png)
+
+7.
+
+```update member set name='test2' where username='test';```
+
+![](./screenshots/Task3_07.png)
+
+### Task 4
+
+1.
+
+`select count(*) from member;`
+
+![](./screenshots/Task4_01.png)
+
+2.
+
+`select sum(follower_count) from member;`
+
+![](./screenshots/Task4_02.png)
+
+3.
+
+`select avg(follower_count) from member;`
+
+![](./screenshots/Task4_03.png)
+
+### Task 5
+
+1.
+```
+CREATE TABLE message(
+    id bigint primary key auto_increment, 
+    member_id bigint not null, 
+    content varchar(255) not null, 
+    like_count int unsigned not null default 0,
+    time datetime not null default current_timestamp
+    );
+```
+
+![](./screenshots/Task5_00.png)
+
+2.
+
+`select member.name, message.content from member inner join message on member.id=message.member_id;`
+
+![](./screenshots/Task5_01.png)
+
+3.
+
+```select member.name, message.content from member inner join message on member.id=message.member_id where user name='test';```
+
+![](./screenshots/Task5_02.png)
+
+4.
+
+```select avg(like_count) from member inner join message on member.id=message.member_id where username='test';```
+
+![](./screenshots/Task5_04.png)
---
+++ week-5/data.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
+--
+-- Host: localhost    Database: website
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `follower_count` int unsigned NOT NULL DEFAULT '0',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'Amy','test','test',100,'2008-08-08 22:10:40'),(2,'Ben','aaa','aaa',150,'2011-02-11 11:18:20'),(3,'Charlie','bbb','bbb',20,'2003-12-01 03:55:50'),(4,'Doris','ccc','ccc',450,'2020-01-01 15:35:10'),(5,'Eric','ddd','ddd',80,'2009-03-03 09:30:00');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `member_id` bigint NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `like_count` int unsigned NOT NULL DEFAULT '0',
+  `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `message_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+INSERT INTO `message` VALUES (1,1,'Good job!',20,'2009-11-23 03:02:16'),(2,2,'Amazing!',130,'2014-04-22 20:42:46'),(3,1,'Well done!',500,'2019-01-22 11:23:33'),(4,3,'Fantastic!',15,'2009-07-30 23:02:10'),(5,4,'Happy birthday!',800,'2022-09-18 23:35:04'),(6,1,'Nice to see you!',200,'2019-12-20 23:32:36'),(7,5,'Stay in touch!',70,'2010-08-22 19:42:48');
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-18 16:57:01
